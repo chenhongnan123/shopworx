c7f3247f9e5a72bfe11d9adddded55793d56c65d
Ankit Jain <ajain@entrib.com>
2020-02-28 09:20:32 +0530

    refactor(assets): Refactored assets to independent package
    Common assets can now be used accross multiple packages without
    redundancy

---
+++ packages/assets/theme/index.js
@@ -0,0 +1,26 @@
+export default {
+  dark: false,
+  options: {
+    customProperties: true,
+  },
+  themes: {
+    light: {
+      primary: '#354493',
+      secondary: '#21C77C',
+      accent: '#01C1E2',
+      error: '#FF5252',
+      info: '#607D8B',
+      success: '#4CAF50',
+      warning: '#FFC107',
+    },
+    dark: {
+      primary: '#21C77C',
+      secondary: '#354493',
+      accent: '#01C1E2',
+      error: '#FF5252',
+      info: '#607D8B',
+      success: '#4CAF50',
+      warning: '#FFC107',
+    },
+  },
+};
