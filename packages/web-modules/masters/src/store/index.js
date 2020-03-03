b3addcf8a73846fcfdd73fc54369a85b7eb13bb7
Ankit Jain <ajain@entrib.com>
2020-03-03 17:47:27 +0530

    refactor(masters): Added API call to fetch provisioning elements

--- packages/web-modules/masters/src/store/index.js
+++ packages/web-modules/masters/src/store/index.js
@@ -1,3 +1,39 @@
+import ElementService from '@shopworx/services/api/element.service';
+import { set } from '@shopworx/services/util/store.service';
+
 export default ({
   namespaced: true,
+  state: {
+    elements: [],
+  },
+  mutations: {
+    setElements: set('elements'),
+  },
+  actions: {
+    getElements: async ({ commit, rootState }) => {
+      const { activeSite } = rootState.user;
+      const { data, status } = await ElementService.getElementsBySite(activeSite);
+      if (status === 200) {
+        console.log(data.results);
+        commit('setElements', data.results);
+      }
+    },
+  },
+  getters: {
+    formattedElements: ({ elements }) => {
+      let list = [];
+      if (elements && elements.length) {
+        const elems = elements
+          .map((elem) => elem.element)
+          .filter((elem) => elem.elementType.toUpperCase().trim() === 'PROVISIONING')
+          .reduce((acc, cur) => {
+            acc[cur.categoryType] = [...acc[cur.categoryType] || [], cur];
+            return acc;
+          }, {});
+        console.log(elems);
+        list = [];
+      }
+      return list;
+    },
+  },
 });
--- packages/web-modules/masters/src/views/Masters.vue
+++ packages/web-modules/masters/src/views/Masters.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     Masters
+    {{formattedElements}}
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
+
 export default {
   name: 'Masters',
+  async created() {
+    await this.getElements();
+  },
+  computed: {
+    ...mapGetters('masters', ['formattedElements']),
+  },
+  methods: {
+    ...mapActions('masters', ['getElements']),
+  },
 };
 </script>
