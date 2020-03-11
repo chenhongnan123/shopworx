2d9d4459ca63809459c9bb112b340d28d9cc4a38
Ankit Jain <ajain@entrib.com>
2020-03-11 17:24:18 +0530

    fix(onboarding): Added validations for business hour provisioning
    Closes QA0-I29, QA0-I30, QA0-I31

--- packages/web-modules/data-onboarding/src/comopnents/calendar/BusinessHours.vue
+++ packages/web-modules/data-onboarding/src/comopnents/calendar/BusinessHours.vue
@@ -1,6 +1,10 @@
 <template>
   <v-card flat class="transparent">
     <v-card-text class="pa-0">
+      <p
+        class="mt-1 text-justify"
+        v-text="$t('onboarding.steps.calendar.businessHoursNote')"
+      ></p>
       <v-row v-for="(rountine, index) in routines" :key="index">
         <v-col cols="12" md="3">
           <v-select
@@ -70,6 +74,8 @@
 </template>
 
 <script>
+import { mapMutations } from 'vuex';
+
 export default {
   name: 'BusinessHours',
   props: {
@@ -102,6 +108,7 @@ export default {
     };
   },
   methods: {
+    ...mapMutations('helper', ['setAlert']),
     addRoutine() {
       this.routines.push({
         type: 'shift',
@@ -116,25 +123,58 @@ export default {
     getStartTime(time) {
       return `${time}:00`;
     },
+    getDuration({ endtime, starttime }) {
+      const timeStart = new Date(`01/01/2007 ${starttime}`).getHours();
+      const timeEnd = new Date(`01/01/2007 ${endtime}`).getHours();
+      return timeEnd - timeStart;
+    },
     getEndTime(time) {
       const [hours, mins] = time.split(':');
       const actualSecs = '59';
       const actualMins = mins !== '00' ? parseInt(mins, 10) - 1 : '59';
-      const actualHour = mins !== '00' ? parseInt(hours, 10) : parseInt(hours, 10) - 1;
+      let actualHour = mins !== '00' ? parseInt(hours, 10) : parseInt(hours, 10) - 1;
+      if (actualHour === -1) {
+        actualHour = 23;
+      }
       const actualTime = [actualHour, actualMins, actualSecs];
       return actualTime.join(':');
     },
+    isHourMissing() {
+      return this.routines.some((routine, index) => (
+        index !== this.routines.length - 1
+        && routine.endtime !== this.routines[index + 1].starttime
+      ));
+    },
+    isSumInvalid() {
+      const sum = this.routines.reduce((acc, cur) => acc + (this.getDuration(cur) || 0), 0);
+      console.log(!!sum);
+      return !!sum;
+    },
     saveHours() {
-      const records = this.routines.map((routine) => ({
-        ...routine,
-        starttime: this.getStartTime(routine.starttime),
-        endtime: this.getEndTime(routine.endtime),
-      }));
-      const payload = {
-        ...this.category,
-        records,
-      };
-      this.$emit('hours-provisioned', payload);
+      if (this.isHourMissing()) {
+        this.setAlert({
+          show: true,
+          type: 'error',
+          message: 'MISSING_BUSINESS_HOUR',
+        });
+      } else if (this.isSumInvalid()) {
+        this.setAlert({
+          show: true,
+          type: 'error',
+          message: 'INVALID_HOURS_SUM',
+        });
+      } else {
+        const records = this.routines.map((routine) => ({
+          ...routine,
+          starttime: this.getStartTime(routine.starttime),
+          endtime: this.getEndTime(routine.endtime),
+        }));
+        const payload = {
+          ...this.category,
+          records,
+        };
+        this.$emit('hours-provisioned', payload);
+      }
     },
   },
 };
