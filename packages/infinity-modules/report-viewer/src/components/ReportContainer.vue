ddac894d4921143a517223245c80505ba973e3fb
Ankit Jain <ajain@entrib.com>
2020-05-04 00:12:16 +0530

    refactor(reports): added highcharts

--- packages/infinity-modules/report-viewer/src/components/ReportContainer.vue
+++ packages/infinity-modules/report-viewer/src/components/ReportContainer.vue
@@ -2,8 +2,8 @@
   <v-container fluid class="py-0">
     <v-row justify="center">
       <v-col cols="12" xl="10" class="py-0">
-        <report-chart></report-chart>
-        <report-list></report-list>
+        <report-chart />
+        <report-grid />
       </v-col>
     </v-row>
   </v-container>
@@ -12,13 +12,13 @@
 <script>
 import { mapState, mapActions } from 'vuex';
 import ReportChart from './ReportChart.vue';
-import ReportList from './ReportList.vue';
+import ReportGrid from './ReportGrid.vue';
 
 export default {
   name: 'ReportContainer',
   components: {
     ReportChart,
-    ReportList,
+    ReportGrid,
   },
   data() {
     return {
