9856a81a4f54ee339b2ea718beb051cbfbd31ad8
Aniket Shinde <ashinde@entrib.com>
2020-06-05 12:22:16 +0530

    refactor(Material Management): Added new screens for Add BOM and Add Meterial

---
+++ packages/infinity-modules/material-management/src/components/AddBom.vue
@@ -0,0 +1,167 @@
+<template>
+  <v-dialog
+    persistent
+    scrollable
+    v-model="dialog"
+    max-width="500px"
+    transition="dialog-transition"
+  >
+    <v-card>
+      <v-card-title primary-title>
+        <span>
+          Create Bom
+        </span>
+        <v-spacer></v-spacer>
+        <v-btn icon small @click="dialog = false">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-card-title>
+        <v-card-text style="height: 200px;">
+        <v-form
+          ref="form"
+          v-model="valid"
+          lazy-validation
+        >
+          <v-text-field
+              :disabled="saving"
+              :rules="rules.name"
+              label="Bom"
+              prepend-icon="mdi-tray-plus"
+              v-model="bomObj.name"
+          ></v-text-field>
+          <v-text-field
+              :disabled="saving"
+              :rules="rules.bomnumber"
+              type="number"
+              label="Bom Number"
+              prepend-icon="mdi-tray-plus"
+              v-model="bomObj.bomnumber"
+          ></v-text-field>
+        </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            class="text-none"
+            :loading="saving"
+            @click="saveBom"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import {
+  mapActions,
+  mapState,
+  // mapGetters,
+  mapMutations,
+} from 'vuex';
+
+export default {
+  data() {
+    return {
+      saving: false,
+      bomObj: {
+      },
+      valid: true,
+      rules: {
+        line: [
+          (v) => !!v || 'Line is required',
+        ],
+        subline: [
+          (v) => !!v || 'Subline is required',
+        ],
+        name: [
+          (v) => !!v || 'Bom is required',
+        ],
+        bomnumber: [
+          (v) => !!v || 'Bom Number is required',
+          (v) => v >= 0 || 'Bom Number is bigger than 0',
+        ],
+      },
+    };
+  },
+  computed: {
+    ...mapState('bomManagement', ['addBomDialog', 'bomList', 'lineList', 'sublineList', 'categoryList', 'lineValue', 'sublineValue']),
+    dialog: {
+      get() {
+        return this.addBomDialog;
+      },
+      set(val) {
+        this.setaddBomDialog(val);
+      },
+    },
+  },
+  created() {
+    // await this.getPageDataList();
+  },
+  methods: {
+    ...mapMutations('helper', ['setAlert']),
+    ...mapMutations('bomManagement', ['setaddBomDialog']),
+    ...mapActions('bomManagement', ['getBomListRecords', 'createBom', 'getSublineList']),
+    handleChangeLine(obj) {
+      console.log(obj);
+      const query = `?query=lineid==${obj.id}`;
+      this.getSublineList(query);
+    },
+    async saveBom() {
+      const { bomObj } = this;
+      console.log(this.bomObj);
+      if (this.$refs.form.validate()) {
+        console.log(this.$refs.form.validate());
+        const { name, bomnumber } = bomObj;
+        if (this.bomList.length) {
+          if (this.bomList.some((bom) => name === bom.name)) {
+            this.setAlert({
+              show: true,
+              type: 'error',
+              message: 'BOM_NAME_PRESENT',
+            });
+            return;
+          }
+          if (this.bomList.some((bom) => bomnumber === bom.bomnumber)) {
+            this.setAlert({
+              show: true,
+              type: 'error',
+              message: 'BOM_NUMBER_PRESENT',
+            });
+            return;
+          }
+        }
+        console.log(bomObj, 'bomObj');
+        const payload = {
+          ...bomObj,
+          assetid: 4,
+          lineid: this.lineValue,
+          sublineid: this.sublineValue,
+        };
+        console.log(payload);
+        this.saving = true;
+        const bomList = await this.createBom(payload);
+        this.saving = false;
+        console.log(bomList);
+        if (bomList) {
+          this.getBomListRecords('');
+          this.setAlert({
+            show: true,
+            type: 'success',
+            message: 'CREATED_BOM',
+          });
+        } else {
+          this.setAlert({
+            show: true,
+            type: 'error',
+            message: 'ERROR_CREATING_BOM',
+          });
+        }
+        this.dialog = false;
+      }
+    },
+  },
+};
+</script>
---
+++ packages/infinity-modules/material-management/src/components/AddMaterial.vue
@@ -0,0 +1,251 @@
+<template>
+  <v-dialog
+    persistent
+    scrollable
+    v-model="dialog"
+    max-width="500px"
+    transition="dialog-transition"
+  >
+    <v-card>
+      <v-card-title primary-title>
+        <span>
+          Create Material
+        </span>
+        <v-spacer></v-spacer>
+        <v-btn icon small @click="dialog = false">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-card-title>
+        <v-card-text>
+        <v-form
+          ref="form"
+          v-model="valid"
+          lazy-validation
+        >
+          <v-autocomplete
+            clearable
+            label="Line"
+            :items="lineList"
+            return-object
+            :disabled="saving"
+            item-text="name"
+            prepend-icon="$production"
+            v-model="materialObj.line"
+            :rules="rules.line"
+            @change="handleChangeLine"
+          >
+            <template v-slot:item="{ item }">
+              <v-list-item-content>
+                <v-list-item-title v-text="item.name"></v-list-item-title>
+                <v-list-item-subtitle v-text="item.id"></v-list-item-subtitle>
+              </v-list-item-content>
+            </template>
+          </v-autocomplete>
+          <v-autocomplete
+            clearable
+            label="Subine"
+            :items="sublineList"
+            return-object
+            :disabled="saving"
+            item-text="name"
+            prepend-icon="$production"
+            v-model="materialObj.subline"
+            :rules="rules.subline"
+          >
+            <template v-slot:item="{ item }">
+              <v-list-item-content>
+                <v-list-item-title v-text="item.name"></v-list-item-title>
+                <v-list-item-subtitle v-text="item.id"></v-list-item-subtitle>
+              </v-list-item-content>
+            </template>
+          </v-autocomplete>
+          <v-text-field
+              :disabled="saving"
+              :rules="rules.name"
+              label="Material"
+              prepend-icon="mdi-tray-plus"
+              v-model="materialObj.name"
+          ></v-text-field>
+          <v-text-field
+              :disabled="saving"
+              :rules="rules.materialnumber"
+              type="number"
+              label="Material Number"
+              prepend-icon="mdi-tray-plus"
+              v-model="materialObj.materialnumber"
+          ></v-text-field>
+          <v-autocomplete
+            clearable
+            label="Category"
+            :items="categoryList"
+            return-object
+            :disabled="saving"
+            item-text="name"
+            prepend-icon="$production"
+            v-model="materialObj.materilcategory"
+            :rules="rules.materilcategory"
+          >
+            <template v-slot:item="{ item }">
+              <v-list-item-content>
+                <v-list-item-title v-text="item.name"></v-list-item-title>
+                <v-list-item-subtitle v-text="item.id"></v-list-item-subtitle>
+              </v-list-item-content>
+            </template>
+          </v-autocomplete>
+          <v-text-field
+              :disabled="saving"
+              :rules="rules.lifetime"
+              label="Lifetime(days)"
+              prepend-icon="mdi-tray-plus"
+              v-model="materialObj.lifetime"
+          ></v-text-field>
+          <v-text-field
+              :disabled="saving"
+              :rules="rules.materialtype"
+              label="Material TypeID"
+              prepend-icon="mdi-tray-plus"
+              v-model="materialObj.materialtype"
+          ></v-text-field>
+          <v-text-field
+              :disabled="saving"
+              :rules="rules.manufacturer"
+              label="Manufacturer"
+              prepend-icon="mdi-tray-plus"
+              v-model="materialObj.manufacturer"
+          ></v-text-field>
+        </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            class="text-none"
+            :loading="saving"
+            @click="saveMaterial"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import {
+  mapActions,
+  mapState,
+  // mapGetters,
+  mapMutations,
+} from 'vuex';
+
+export default {
+  data() {
+    return {
+      saving: false,
+      materialObj: {
+      },
+      valid: true,
+      rules: {
+        line: [
+          (v) => !!v || 'Line is required',
+        ],
+        subline: [
+          (v) => !!v || 'Subline is required',
+        ],
+        name: [
+          (v) => !!v || 'Material is required',
+        ],
+        materialnumber: [
+          (v) => !!v || 'Material Number is required',
+          (v) => v >= 0 || 'Material Number is bigger than 0',
+        ],
+        materilcategory: [
+          (v) => !!v || 'Category is required',
+        ],
+        lifetime: [
+          (v) => !!v || 'lifetime is required',
+        ],
+        materialtype: [
+          (v) => !!v || 'Materialtype Typeid is required',
+        ],
+        manufacturer: [
+          (v) => !!v || 'Manufacturer is required',
+        ],
+      },
+    };
+  },
+  computed: {
+    ...mapState('materialManagement', ['addMaterialDialog', 'materialList', 'lineList', 'sublineList', 'categoryList', 'lineValue', 'sublineValue']),
+    dialog: {
+      get() {
+        return this.addMaterialDialog;
+      },
+      set(val) {
+        this.setaddMaterialDialog(val);
+      },
+    },
+  },
+  created() {
+    // await this.getPageDataList();
+  },
+  methods: {
+    ...mapMutations('helper', ['setAlert']),
+    ...mapMutations('materialManagement', ['setaddMaterialDialog']),
+    ...mapActions('materialManagement', ['getMaterialListRecords', 'createMaterial', 'getSublineList']),
+    handleChangeLine(obj) {
+      console.log(obj);
+      const query = `?query=lineid==${obj.id}`;
+      this.getSublineList(query);
+    },
+    async saveMaterial() {
+      const { materialObj } = this;
+      console.log(this.materialObj);
+      if (this.$refs.form.validate()) {
+        console.log(this.$refs.form.validate());
+        const { name, materialnumber } = materialObj;
+        if (this.materialList.length) {
+          if (this.materialList.some((material) => name === material.name)) {
+            this.setAlert({
+              show: true,
+              type: 'error',
+              message: 'MATERIAL_NAME_PRESENT',
+            });
+            return;
+          }
+          if (this.materialList.some((material) => materialnumber === material.materialnumber)) {
+            this.setAlert({
+              show: true,
+              type: 'error',
+              message: 'MATERIAL_NUMBER_PRESENT',
+            });
+            return;
+          }
+        }
+        const payload = {
+          ...materialObj,
+          assetid: 4,
+          materilcategory: materialObj.materilcategory.id,
+        };
+        this.saving = true;
+        const materialList = await this.createMaterial(payload);
+        this.saving = false;
+        if (materialList) {
+          this.getMaterialListRecords('');
+          this.setAlert({
+            show: true,
+            type: 'success',
+            message: 'CREATED_MATERIAL',
+          });
+        } else {
+          this.setAlert({
+            show: true,
+            type: 'error',
+            message: 'ERROR_CREATING_MATERIAL',
+          });
+        }
+        this.dialog = false;
+      }
+    },
+  },
+};
+</script>
