ee765c14fd12ff11fab24a4c6121f3dc0f8f0516
Mayur Pathak <271531@ODCDPW.IC>
2020-06-09 17:21:23 +0530

    refactor(basic validations): validations for empty field duplicate entry

--- packages/infinity-modules/production-layout/src/Components/UpdateProcess.vue
+++ packages/infinity-modules/production-layout/src/Components/UpdateProcess.vue
@@ -4,23 +4,36 @@
     <v-icon v-on="on" v-text="'$edit'" color="primary"
     class="float-right"></v-icon>
     </template>
+    <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation>
     <v-card>
     <v-card-title>
         <span class="headline">Update Process</span>
          <v-spacer></v-spacer>
-          <v-btn icon small @click="dialog = false">
+          <v-btn icon small @click="(dialog = false); resetDialog();">
           <v-icon>mdi-close</v-icon>
         </v-btn>
     </v-card-title>
     <v-card-text>
       <v-row>
       <v-col cols="12" md="12">
-        <v-text-field label="Name" v-model="processNew.name" required
+        <v-text-field label="Name" v-model="processNew.name"
+          :rules="nameRules"
+          :counter="15"
+           required
+           hint="For example, Process_01"
           @keyup="nameValid" ></v-text-field>
          <v-text-field label="Number" type="number"
-          v-model="processNew.numbers" required
+          v-model="processNew.numbers"
+          :rules="numberRules"
+          :counter="10"
+           required
+           hint="For example, 1231"
           @keyup="numberValid"></v-text-field>
         <v-text-field label="Description" type="Description"
+        hint="For example, Updated by Manager"
          v-model="processNew.description"></v-text-field>
       </v-col>
       </v-row>
@@ -33,6 +46,7 @@
         @click="saveProcess">Save</v-btn>
     </v-card-actions>
     </v-card>
+    </v-form>
 </v-dialog>
 </template>
 <script>
@@ -50,9 +64,19 @@ export default {
       dialog: false,
       processNew: {},
       btnDisable: false,
+      valid: true,
+      name: '',
+      numbers: '',
+      numberRules: [(value) => !!value || 'Number required',
+        (v) => (v && v.length <= 10) || 'Number must be less than 10 characters',
+      ],
+      nameRules: [(value) => !!value || 'Name required',
+        (v) => (v && v.length <= 15) || 'Name must be less than 15 characters',
+      ],
     };
   },
   created() {
+    console.log(this.process);
     this.processNew = { ...this.process };
   },
   computed: {
@@ -62,31 +86,39 @@ export default {
     ...mapMutations('helper', ['setAlert']),
     ...mapActions('productionLayout', ['updateProcess']),
     async nameValid() {
-      const processNameFlag = this.processes
-        .filter((o) => o.name.toLowerCase().split(' ').join('') === this.processNew.name.toLowerCase().split(' ').join(''));
-      if (processNameFlag.length > 0) {
+      if (this.processNew.name === '' || this.processNew.name.length > 15) {
         this.btnDisable = true;
-        this.setAlert({
-          show: true,
-          type: 'error',
-          message: 'ALREADY_EXSIST',
-        });
       } else {
-        this.btnDisable = false;
+        const processNameFlag = this.processes
+          .filter((o) => o.name.toLowerCase().split(' ').join('') === this.processNew.name.toLowerCase().split(' ').join(''));
+        if (processNameFlag.length > 0) {
+          this.btnDisable = true;
+          this.setAlert({
+            show: true,
+            type: 'error',
+            message: 'ALREADY_EXSIST',
+          });
+        } else {
+          this.btnDisable = false;
+        }
       }
     },
     async numberValid() {
-      const processNumberFlag = this.processes
-        .filter((o) => o.numbers === this.processNew.numbers);
-      if (processNumberFlag.length > 0) {
+      if (this.processNew.numbers === '' || this.processNew.numbers.length > 10) {
         this.btnDisable = true;
-        this.setAlert({
-          show: true,
-          type: 'error',
-          message: 'ALREADY_EXSIST_NO',
-        });
       } else {
-        this.btnDisable = false;
+        const processNumberFlag = this.processes
+          .filter((o) => o.numbers === this.processNew.numbers);
+        if (processNumberFlag.length > 0) {
+          this.btnDisable = true;
+          this.setAlert({
+            show: true,
+            type: 'error',
+            message: 'ALREADY_EXSIST_NO',
+          });
+        } else {
+          this.btnDisable = false;
+        }
       }
     },
     async saveProcess() {
@@ -102,6 +134,7 @@ export default {
         payload: this.newProcess,
         substationid: this.process.substationid,
       };
+      console.log(payload);
       created = this.updateProcess(payload);
       if (created) {
         this.setAlert({
@@ -111,7 +144,6 @@ export default {
         });
         this.dialog = false;
         this.assetId = 4;
-        // this.newProcess = {};
       } else {
         this.setAlert({
           show: true,
@@ -120,6 +152,10 @@ export default {
         });
       }
       this.saving = false;
+    },
+    async resetDialog() {
+      this.$refs.form.resetValidation();
+      this.sublineNew = { ...this.subline };
     },
   },
 };
