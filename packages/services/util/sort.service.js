c1867cb78029793385c715ebeaa4e2b1308e1f80
Ankit Jain <ajain@entrib.com>
2020-10-25 15:42:03 +0530

    feat(sort): added alpha numeric sorting helper

---
+++ packages/services/util/sort.service.js
@@ -0,0 +1,10 @@
+export const sortAlphaNum = (a, b) => {
+  const aA = a.replace(/[^a-zA-Z]/g, '');
+  const bA = b.replace(/[^a-zA-Z]/g, '');
+  if (aA === bA) {
+    const aN = parseInt(a.replace(/[^0-9]/g, ''), 10);
+    const bN = parseInt(b.replace(/[^0-9]/g, ''), 10);
+    return aN === bN ? 0 : aN > bN ? 1 : -1;
+  }
+  return aA > bA ? 1 : -1;
+}
