03174bc9ad007f7c1055eec302545921895a2798
Ankit Jain <ajain@entrib.com>
2020-11-12 09:53:01 +0530

    refactor(model-management): added confirmation boxes

--- packages/infinity-modules/model-management/src/components/DeleteModel.vue
+++ packages/infinity-modules/model-management/src/components/DeleteModel.vue
@@ -3,9 +3,10 @@
     <template v-slot:activator="{ on, attrs }">
       <v-btn
         icon
+        v-on="on"
         color="error"
         v-bind="attrs"
-        v-on="on"
+        @click="deleteModel"
       >
         <v-icon>mdi-delete-outline</v-icon>
       </v-btn>
@@ -17,5 +18,28 @@
 <script>
 export default {
   name: 'DeleteModel',
+  props: {
+    model: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    async deleteModel() {
+      if (await this.$root.$confirm.open(
+        'Delete model',
+        `Are you sure want to delete "${this.model.modelname}"?
+        This will also delete the associated model files.`,
+      )) {
+        /* await Promise.all([
+          this.plans.forEach((plan) => {
+            // eslint-disable-next-line
+            this.deletePlan(plan._id);
+          }),
+        ]); */
+        this.$emit('on-delete');
+      }
+    },
+  },
 };
 </script>
--- packages/infinity-modules/model-management/src/components/DeployModel.vue
+++ packages/infinity-modules/model-management/src/components/DeployModel.vue
@@ -3,9 +3,10 @@
     <template v-slot:activator="{ on, attrs }">
       <v-btn
         icon
-        color="success"
-        v-bind="attrs"
         v-on="on"
+        v-bind="attrs"
+        color="success"
+        @click="deployModel"
       >
         <v-icon>mdi-rocket-outline</v-icon>
       </v-btn>
@@ -17,5 +18,28 @@
 <script>
 export default {
   name: 'DeployModel',
+  props: {
+    model: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    async deployModel() {
+      if (await this.$root.$confirm.open(
+        'Deploy model',
+        `Please confirm the deployment for "${this.model.modelname}".
+        You cannot stop the deployment in the middle once it is started.`,
+      )) {
+        /* await Promise.all([
+          this.plans.forEach((plan) => {
+            // eslint-disable-next-line
+            this.deletePlan(plan._id);
+          }),
+        ]); */
+        this.$emit('on-deploy');
+      }
+    },
+  },
 };
 </script>
