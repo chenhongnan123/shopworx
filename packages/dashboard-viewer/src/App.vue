5a68d34ff0f7c6ef51fd2882f6940893e611817c
Ankit Jain <ajain@entrib.com>
2020-11-22 00:14:47 +0530

    refactor(dashboard-viewer): scaffolded dashboard viewer

---
+++ packages/dashboard-viewer/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <div id="app">
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
