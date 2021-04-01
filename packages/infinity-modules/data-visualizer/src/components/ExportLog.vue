304b4bda22d857fe88d43c8fbee0a3685db5ad15
Ankit Jain <ajain@entrib.com>
2021-04-01 19:35:03 +0530

    feat(data-visualizer): added date query in the export logs

--- packages/infinity-modules/data-visualizer/src/components/ExportLog.vue
+++ packages/infinity-modules/data-visualizer/src/components/ExportLog.vue
@@ -14,12 +14,22 @@
       :items="downloadLogs"
       item-key="_id"
       :loading="fetchingLogs"
-    ></v-data-table>
+    >
+      <!-- eslint-disable-next-line -->
+      <template #item.dateFrom="{ item }">
+        <div>{{ getDateFrom(item.query) }}</div>
+      </template>
+      <!-- eslint-disable-next-line -->
+      <template #item.dateTo="{ item }">
+        <div>{{ getDateTo(item.query) }}</div>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
+import { formatDate } from '@shopworx/services/util/date.service';
 
 export default {
   name: 'ExportLog',
@@ -27,20 +37,37 @@ export default {
     return {
       headers: [
         { text: 'Requested at', value: 'createdTimestamp' },
+        { text: 'Requested by', value: 'createdBy' },
         { text: 'Element name', value: 'elementName' },
         { text: 'Fields', value: 'fields' },
-        { text: 'Query', value: 'query' },
+        { text: 'Date from', value: 'dateFrom' },
+        { text: 'Date to', value: 'dateTo' },
         { text: 'Status', value: 'status' },
         { text: 'Email sent?', value: 'isEmailSent' },
         { text: 'Download link', value: 'link' },
       ],
     };
   },
+  created() {
+    this.getDownloadLog();
+  },
   computed: {
     ...mapState('dataVisualizer', ['downloadLogs', 'fetchingLogs']),
   },
   methods: {
     ...mapActions('dataVisualizer', ['getDownloadLog']),
+    parseDateRange(query) {
+      const condition = JSON.parse(query);
+      return condition.$and[1].createdTimestamp;
+    },
+    getDateFrom(query) {
+      const timestamps = this.parseDateRange(query);
+      return formatDate(new Date(timestamps.$gte.$date), 'yyyy-MM-dd HH:mm:ss');
+    },
+    getDateTo(query) {
+      const timestamps = this.parseDateRange(query);
+      return formatDate(new Date(timestamps.$lte.$date), 'yyyy-MM-dd HH:mm:ss');
+    },
   },
 };
 </script>
