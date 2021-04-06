1207910b941a64fcce2cb35a3364619b98f1f8d5
Ankit Jain <ajain@entrib.com>
2021-04-06 13:43:37 +0530

    fix(origin): fixed object parameter for shift oee report

--- packages/origin-modules/customer-add-new/src/data/reportsPayload.js
+++ packages/origin-modules/customer-add-new/src/data/reportsPayload.js
@@ -352,6 +352,7 @@ export default [
         reportObjectDesc: 'performance',
         reportObjectParameters: [
           'siteid',
+          'dateVal',
           'shiftVal',
         ],
       },
