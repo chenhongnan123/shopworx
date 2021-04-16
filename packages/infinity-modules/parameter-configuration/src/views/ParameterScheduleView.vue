24c0401e00946f7e874eb34a329dcc056ba40d43
MayurPathak <mpathak@entrib.com>
2021-04-16 18:04:57 +0530

    style(parameter-configuration): RA-I566 horizontal scrollbar

--- packages/infinity-modules/parameter-configuration/src/views/ParameterScheduleView.vue
+++ packages/infinity-modules/parameter-configuration/src/views/ParameterScheduleView.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid class="py-0">
+  <v-container fluid class="py-0" v-resize="setHeight">
     <div justify="center" class="planScheduleView">
       <div class="py-0">
         <div class="stick">
@@ -156,6 +156,8 @@
         'items-per-page-options': [100, 300, 500, 1000]}"
         :items-per-page="100"
         show-select
+        fixed-header
+        :height="height"
         >
           <template #item.name="props">
             <v-edit-dialog
@@ -500,9 +502,11 @@ export default {
       socket: null,
       saving: false,
       savingImport: false,
+      height: window.innerHeight,
     };
   },
   async mounted() {
+    this.setHeight();
     this.ProceedDialog = this.$refs.ProceedDialog;
     this.$root.$on('confirmationSignal', (data) => {
       this.dialog = data;
@@ -604,6 +608,9 @@ export default {
     ...mapMutations('parameterConfiguration', ['setAddParameterDialog', 'toggleFilter', 'setLineValue', 'setSublineValue', 'setStationValue', 'setSubstationValue', 'setSelectedParameterName', 'setSelectedParameterDirection', 'setSelectedParameterCategory', 'setSelectedParameterDatatype', 'setCreateParam']),
     ...mapActions('parameterConfiguration', ['getPageDataList', 'getSublineList', 'getStationList', 'getSubstationList', 'getParameterListRecords', 'updateParameter', 'deleteParameter', 'createParameter', 'createParameterList', 'downloadToPLC', 'getSubStationIdElement',
       'getSubStationIdElement', 'createTagElement', 'updateTagStatus', 'getParametersList']),
+    setHeight() {
+      this.height = window.innerHeight - 212;
+    },
     async executeCreateFunction(val) {
       if (val) {
         this.responce = [];
