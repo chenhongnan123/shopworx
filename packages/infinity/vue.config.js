5d4e879e65f1ea8b0776feb2f370d5473057ea50
MayurPathak <mpathak@entrib.com>
2021-05-13 11:08:55 +0530

    fix(recipe-management): RA-I152 | Recipe Management | User is able to add recipe without selection of Subline and station

--- packages/infinity/vue.config.js
+++ packages/infinity/vue.config.js
@@ -5,14 +5,14 @@ module.exports = {
   devServer: {
     proxy: {
       '/server': {
-        target: 'http://192.168.1.169/server',
+        target: 'http://35.154.51.34/server',
         changeOrigin: true,
         pathRewrite: {
           '^/server': '',
         },
       },
       '/sse': {
-        target: 'http://192.168.1.169/sse',
+        target: 'http://35.154.51.34/sse',
         changeOrigin: true,
         pathRewrite: {
           '^/sse': '',
