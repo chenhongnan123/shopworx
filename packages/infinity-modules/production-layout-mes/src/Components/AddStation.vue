9360360a8e6631547459bb4e0afc4ea845d99f0f
MayurPathak <mpathak@entrib.com>
2021-05-17 13:48:46 +0530

    fix(production-layout): RA-I491 | Line MES | Production Layout | User can add blank spaces as name for every field

--- packages/infinity-modules/production-layout-mes/src/Components/AddStation.vue
+++ packages/infinity-modules/production-layout-mes/src/Components/AddStation.vue
@@ -122,7 +122,8 @@ export default {
       numberRules: [(v) => (v && v.length) > 0 || 'number required',
         (v) => (v && v.length <= 10) || 'Number must be less than 10 characters'],
       nameRules: [(v) => !!v || 'Name required',
-        (v) => (v && v.length <= 15) || 'Name must be less than 15 characters'],
+        (v) => (v && v.length <= 15) || 'Name must be less than 15 characters',
+        (v) => /^\S+$/.test(v) || 'Space not allowed'],
       plcRules: [(v) => /^(([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])(\.(?!$)|(?=$))){4}$/.test(v) || 'Invalid format'],
     };
   },
